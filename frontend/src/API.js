08c2976ef0d61a9b9dc1bee0206a1df119a9ccf7
iamniteshtiwari <89508196+iamniteshtiwari@users.noreply.github.com>
2021-11-11 07:07:45 +0530

    changes in API

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -2,15 +2,15 @@ import axios from "axios";
 const LOGIN_USER_KEY = "LOGIN_USER_KEY";
 
 var baseURL;
-if (
-  process.env.REACT_APP_ENVIRONMENT &&
-  process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
-) {
-  baseURL = process.env.REACT_APP_API_BASE_URL;
-} else {
-  baseURL = "http://127.0.0.1:8000";
-}
-
+// if (
+//   process.env.REACT_APP_ENVIRONMENT &&
+//   process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
+// ) {
+//   baseURL = process.env.REACT_APP_API_BASE_URL;
+// } else {
+//   baseURL = "http://127.0.0.1:8000";
+// }
+baseURL = "https://backend-choza-de-abarrotes.herokuapp.com";
 const api = axios.create({
   baseURL: baseURL,
   headers: {
