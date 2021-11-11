a873510c917b0bf1da3c1abda668175037e531af
iamniteshtiwari <89508196+iamniteshtiwari@users.noreply.github.com>
2021-11-11 14:33:49 +0530

    added to cart

--- frontend/src/components/Common/MainImage.jsx
+++ frontend/src/components/Common/MainImage.jsx
@@ -47,11 +47,12 @@ const MainImage = () => {
     <div class="card card3">
       <img src={Rect1} class="rect" alt="rect"/>
       <h6 class="card-img2"><a href="cart">Spices</a></h6>
-      <div class="plus1">
-        <i class="bi bi-plus-lg"><a href="cart"></a></i>
+      <div class="plus1"><a href="cart">
+        <i class="bi bi-plus-lg"></i>
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
           <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z"/>
         </svg>
+        </a>
         </div>
     </div>
     <div class="card card4">
